324356c13b912aaa0eeed8236eed7a743abecd46
Hiroaki Kono <hiroakikono1990@gmail.com>
2015-06-19 02:24:43 +0900

    added solution for exercise3

--- src/test/java/stream/api/code/kata/Exercise3Test.java
+++ src/test/java/stream/api/code/kata/Exercise3Test.java
@@ -26,7 +26,7 @@ public class Exercise3Test extends ClassicOnlineStore {
         /**
          * Create a list of customer names using {@link Stream#collect} and {@link Collectors#toList}
          */
-        List<String> nameList = null;
+        List<String> nameList = customerList.stream().map(Customer::getName).collect(Collectors.toList());
 
         assertThat(nameList, contains("Joe", "Steven", "Patrick", "Diana", "Chris", "Kathy", "Alice", "Andrew",
                                       "Martin", "Amy"));
@@ -39,7 +39,7 @@ public class Exercise3Test extends ClassicOnlineStore {
         /**
          * Create a set of customer age using {@link Stream#collect} and {@link Collectors#toSet}
          */
-        Set<Integer> ageSet = null;
+        Set<Integer> ageSet = customerList.stream().map(Customer::getAge).collect(Collectors.toSet());
 
         assertThat(ageSet, hasSize(9));
         assertThat(ageSet, hasItems(21, 22, 26, 27, 28, 32, 35, 36, 38));
@@ -52,7 +52,7 @@ public class Exercise3Test extends ClassicOnlineStore {
         /**
          * Create a csv string of customer names in brackets "[]" using {@link Collectors#joining}
          */
-        String string = null;
+        String string = customerList.stream().map(Customer::getName).collect(Collectors.joining(",", "[", "]"));
 
         assertThat(string, is("[Joe,Steven,Patrick,Diana,Chris,Kathy,Alice,Andrew,Martin,Amy]"));
     }
@@ -64,7 +64,8 @@ public class Exercise3Test extends ClassicOnlineStore {
         /**
          * Get oldest customer by using {@link Collectors#maxBy}. Don't use any intermediate operations.
          */
-        Optional<Customer> oldestCustomer = null;
+        Optional<Customer> oldestCustomer = customerList.stream()
+            .collect(Collectors.maxBy((o1, o2) -> o1.getAge() - o2.getAge()));
 
         assertThat(oldestCustomer.get(), is(customerList.get(3)));
     }
@@ -77,7 +78,8 @@ public class Exercise3Test extends ClassicOnlineStore {
          * Create a map of age as key and number of customers as value
          * using {@link Collectors#groupingBy} and {@link Collectors#counting}
          */
-        Map<Integer, Long> ageDistribution = null;
+        Map<Integer, Long> ageDistribution = customerList.stream()
+            .collect(Collectors.groupingBy(Customer::getAge, Collectors.counting()));
 
         assertThat(ageDistribution.size(), is(9));
         ageDistribution.forEach((k, v) -> {
