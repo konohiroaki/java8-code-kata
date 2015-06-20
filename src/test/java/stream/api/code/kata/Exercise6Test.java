da2f6ca2e8b7650309ca467cca03702220fa3102
Hiroaki Kono <hiroakikono1990@gmail.com>
2015-06-20 09:33:41 +0900

    added exercise6

---
+++ src/test/java/stream/api/code/kata/Exercise6Test.java
@@ -0,0 +1,37 @@
+package stream.api.code.kata;
+
+import org.junit.Test;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import stream.api.code.kata.utils.ClassicOnlineStore;
+
+import static org.hamcrest.Matchers.contains;
+import static org.junit.Assert.*;
+
+public class Exercise6Test extends ClassicOnlineStore {
+
+    @Test
+    public void streamFromValues() {
+        /**
+         * Create a stream with string values "one" "two" "three" using {@link Stream#of}
+         */
+        Stream<String> oneTwoThreeStream = null;
+
+        List<String> oneTwoThreeList = oneTwoThreeStream.collect(Collectors.toList());
+        assertThat(oneTwoThreeList, contains("one", "two", "three"));
+    }
+
+    @Test
+    public void numberStream() {
+        /**
+         * Create a stream only with multiples of 3, starting from 0, size of 10, using {@link Stream#iterate}
+         */
+        Stream<Integer> numbers = null;
+
+        List<Integer> numbersList = numbers.collect(Collectors.toList());
+        assertThat(numbersList, contains(0, 3, 6, 9, 12, 15, 18, 21, 24, 27));
+    }
+}
