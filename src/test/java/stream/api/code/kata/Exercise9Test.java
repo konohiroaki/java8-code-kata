599024adf9ba14dee9c0c9fa166826d8fc7e3560
Hiroaki Kono <hiroakikono1990@gmail.com>
2015-08-01 21:10:26 +0900

    removed unused import

--- src/test/java/stream/api/code/kata/Exercise9Test.java
+++ src/test/java/stream/api/code/kata/Exercise9Test.java
@@ -7,7 +7,6 @@ import java.util.EnumSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.StringJoiner;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
