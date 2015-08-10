564a9f6667fc1a745b628b237d026094c2b09640
Hiroaki Kono <hiroakikono1990@gmail.com>
2015-08-10 23:38:06 +0900

    added date time api exercise1

---
+++ common-test-tool/src/main/java/common/test/tool/dataset/DateAndTimes.java
@@ -0,0 +1,22 @@
+package common.test.tool.dataset;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class DateAndTimes {
+
+    public static final LocalDate ld_20150618 = LocalDate.of(2015, 6, 18);
+    public static final LocalDate ld_20150807 = LocalDate.of(2015, 8, 7);
+    public static final LocalTime lt_23073050 = LocalTime.of(23, 7, 30, 500000000);
+    public static final LocalDateTime ldt_20150618_23073050 = LocalDateTime.of(2015, 6, 18, 23, 7, 30, 500000000);
+    public static final Duration d_23h = Duration.of(23, ChronoUnit.HOURS);
+    public static final Period p_1y5m13d = Period.of(1, 5, 13);
+    public static final ZonedDateTime
+        zdt_20150710_02143050 = ZonedDateTime.of(ldt_20150618_23073050, ZoneId.systemDefault());
+}
---
+++ date-and-time-api/src/test/java/date/and/time/api/Exercise1Test.java
@@ -0,0 +1,143 @@
+package date.and.time.api;
+
+import common.test.tool.dataset.DateAndTimes;
+
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class Exercise1Test {
+
+    @Test
+    public void localDateOf() {
+        /**
+         * Create a {@link LocalDate} of 2015-06-18 by using {@link LocalDate#of}
+         */
+        LocalDate localDate = null;
+
+        assertThat(localDate.toString(), is("2015-06-18"));
+    }
+
+    @Test
+    public void localDateParse() {
+        /**
+         * Create a {@link LocalDate} of 2015-06-18 from String by using {@link LocalDate#parse}
+         */
+        LocalDate localDate = null;
+
+        assertThat(localDate.toString(), is("2015-06-18"));
+    }
+
+    @Test
+    public void localDateWith() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+
+        /**
+         * Create a {@link LocalDate} from {@link ld} with year 2015
+         * by using {@link LocalDate#withYear} or {@link LocalDate#with}
+         */
+        LocalDate localDate = null;
+
+        assertThat(localDate.getYear(), is(2015));
+        assertThat(localDate.getMonth(), is(ld.getMonth()));
+        assertThat(localDate.getDayOfMonth(), is(ld.getDayOfMonth()));
+    }
+
+    @Test
+    public void localDateWithAdjuster() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+
+        /**
+         * Create a {@link LocalDate} from {@link ld} adjusted into first day of next year
+         * by using {@link LocalDate#with} and {@link TemporalAdjusters#firstDayOfNextYear}
+         */
+        LocalDate localDate = null;
+
+        assertThat(localDate.getYear(), is(ld.getYear() + 1));
+        assertThat(localDate.getMonth(), is(Month.JANUARY));
+        assertThat(localDate.getDayOfMonth(), is(1));
+    }
+
+    @Test
+    public void localDatePlus() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+
+        /**
+         * Create a {@link LocalDate} from {@link ld} with 10 month later
+         * by using {@link LocalDate#plusDays} or {@link LocalDate#plus}
+         */
+        LocalDate localDate = null;
+
+        assertThat(localDate.getYear(), is(ld.getYear() + 1));
+        assertThat(localDate.getMonth(), is(Month.APRIL));
+        assertThat(localDate.getDayOfMonth(), is(ld.getDayOfMonth()));
+    }
+
+    @Test
+    public void localDateMinus() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+
+        /**
+         * Create a {@link LocalDate} from {@link ld} with 10 days before
+         * by using {@link LocalDate#minusDays} or {@link LocalDate#minus}
+         */
+        LocalDate localDate = null;
+
+        assertThat(localDate.getYear(), is(ld.getYear()));
+        assertThat(localDate.getMonth(), is(ld.getMonth()));
+        assertThat(localDate.getDayOfMonth(), is(ld.getDayOfMonth() - 10));
+    }
+
+
+    @Test
+    public void localDatePlusPeriod() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+
+        /**
+         * Define a {@link Period} of 1 year 2 month 3 days
+         * Create a {@link LocalDate} adding the period to {@link ld} by using {@link LocalDate#plus}
+         */
+        Period period = null;
+        LocalDate localDate = null;
+
+        assertThat(localDate.getYear(), is(ld.getYear() + 1));
+        assertThat(period.getMonths(), is(2));
+        assertThat(localDate.getDayOfMonth(), is(ld.getDayOfMonth() + 3));
+    }
+
+    @Test
+    public void localDateIsAfter() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+        LocalDate ld2 = DateAndTimes.ld_20150807;
+
+        /**
+         * Check whether {@link ld2} is after {@link ld} or not
+         * by using {@link LocalDate#isAfter} or {@link LocalDate#isBefore}
+         */
+        boolean isAfter0618 = false;
+
+        assertThat(isAfter0618, is(true));
+    }
+
+    @Test
+    public void localDateUntil() {
+        LocalDate ld = DateAndTimes.ld_20150618;
+        LocalDate ld2 = DateAndTimes.ld_20150807;
+
+        /**
+         * Create a period from {@link ld} till {@link ld2}
+         * by using {@link LocalDate#until}
+         */
+        Period period = null;
+
+        assertThat(period.getYears(), is(0));
+        assertThat(period.getMonths(), is(1));
+        assertThat(period.getDays(), is(20));
+    }
+}
