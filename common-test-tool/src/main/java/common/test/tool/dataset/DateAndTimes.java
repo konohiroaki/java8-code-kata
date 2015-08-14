ca9e69cb4880e2ff6bd774d214fc9e213e76dad3
Hiroaki Kono <hiroakikono1990@gmail.com>
2015-08-14 17:59:10 +0900

    added old date api conversion exercise

--- common-test-tool/src/main/java/common/test/tool/dataset/DateAndTimes.java
+++ common-test-tool/src/main/java/common/test/tool/dataset/DateAndTimes.java
@@ -1,5 +1,7 @@
 package common.test.tool.dataset;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -15,4 +17,7 @@ public class DateAndTimes {
     public static final LocalDateTime ldt_20150618_23073050 = LocalDateTime.of(2015, 6, 18, 23, 7, 30, 500000000);
     public static final ZonedDateTime
         zdt_20150618_23073050 = ZonedDateTime.of(ldt_20150618_23073050, ZoneId.of("Asia/Tokyo"));
+
+    public static final Date old_date_20150618 = Date.valueOf("2015-06-18");
+    public static final Timestamp old_timestamp_20150618_23073050 = Timestamp.valueOf("2015-06-18 23:07:30.5");
 }
