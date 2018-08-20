cbb6442075f102f337704aaffbc683db082780e8
Laszlo Igaz <laszlo.igaz@gmail.com>
2018-08-20 19:51:07 +0200

    Solved Stream.Exercise7

--- stream-api/src/test/java/stream/api/Exercise7Test.java
+++ stream-api/src/test/java/stream/api/Exercise7Test.java
@@ -3,6 +3,7 @@ package stream.api;
 import common.test.tool.annotation.Easy;
 import common.test.tool.dataset.ClassicOnlineStore;
 import common.test.tool.entity.Customer;
+import common.test.tool.entity.Item;
 import common.test.tool.entity.Shop;
 
 import org.junit.Test;
@@ -26,8 +27,8 @@ public class Exercise7Test extends ClassicOnlineStore {
          * Create {@link IntStream} with customer ages by using {@link Stream#mapToInt}
          * Then calculate the average of ages by using {@link IntStream#average}
          */
-        IntStream ageStream = null;
-        OptionalDouble average = null;
+        IntStream ageStream = customerList.stream().mapToInt(Customer::getAge);
+        OptionalDouble average = ageStream.average();
 
         assertThat(average.getAsDouble(), is(28.7));
     }
@@ -40,8 +41,8 @@ public class Exercise7Test extends ClassicOnlineStore {
          * Create {@link LongStream} with all items' prices using {@link Stream#mapToLong}
          * Then calculate the sum of prices using {@link LongStream#sum}
          */
-        LongStream priceStream = null;
-        long priceSum = 0;
+        LongStream priceStream = shopList.stream().flatMap(shop -> shop.getItemList().stream()).mapToLong(Item::getPrice);
+        long priceSum = priceStream.sum();
 
         assertThat(priceSum, is(60930L));
     }
