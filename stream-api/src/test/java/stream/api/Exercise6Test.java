3d37d951a8c578e07d481a826d6b9b49c99c58b9
Laszlo Igaz <laszlo.igaz@gmail.com>
2018-08-20 19:47:10 +0200

    Solved Stream.Exercise6

--- stream-api/src/test/java/stream/api/Exercise6Test.java
+++ stream-api/src/test/java/stream/api/Exercise6Test.java
@@ -18,7 +18,7 @@ public class Exercise6Test {
         /**
          * Create a stream with string values "a" "b" "c" by using {@link Stream#of}
          */
-        Stream<String> abcStream = null;
+        Stream<String> abcStream = Stream.of("a", "b", "c");
 
         List<String> abcList = abcStream.collect(Collectors.toList());
         assertThat(abcList, contains("a", "b", "c"));
@@ -29,7 +29,7 @@ public class Exercise6Test {
         /**
          * Create a stream only with multiples of 3, starting from 0, size of 10, by using {@link Stream#iterate}
          */
-        Stream<Integer> numbers = null;
+        Stream<Integer> numbers = Stream.iterate(0, o -> o + 3).limit(10);
 
         List<Integer> numbersList = numbers.collect(Collectors.toList());
         assertThat(numbersList, contains(0, 3, 6, 9, 12, 15, 18, 21, 24, 27));
