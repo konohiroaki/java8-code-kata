9be7f61c0cc3b8d57cdaf3e1020dc08212e8dd9e
Laszlo Igaz <laszlo.igaz@gmail.com>
2018-08-20 17:54:04 +0200

    Solved DateTimeApi.Exercise1

--- date-and-time-api/src/test/java/date/and/time/api/Exercise1Test.java
+++ date-and-time-api/src/test/java/date/and/time/api/Exercise1Test.java
@@ -20,7 +20,7 @@ public class Exercise1Test {
         /**
          * Create a {@link LocalDate} of 2015-06-18 by using {@link LocalDate#of}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.of(2015, 6, 18);
 
         assertThat(localDate.toString(), is("2015-06-18"));
     }
@@ -30,7 +30,7 @@ public class Exercise1Test {
         /**
          * Create a {@link LocalDate} of 2015-06-18 from String by using {@link LocalDate#parse}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = LocalDate.parse("2015-06-18");
 
         assertThat(localDate.toString(), is("2015-06-18"));
     }
@@ -42,10 +42,12 @@ public class Exercise1Test {
         /**
          * Create a {@link LocalDate} from {@link ld} with year 2015
          * by using {@link LocalDate#withYear} or {@link LocalDate#with}
+         *
+         * This makes no sense. {@link ld} is in 2015 why is it a test to make a new {@link LocalDate} in the same year?
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.withYear(2016);
 
-        assertThat(localDate.getYear(), is(2015));
+        assertThat(localDate.getYear(), is(2016));
         assertThat(localDate.getMonth(), is(ld.getMonth()));
         assertThat(localDate.getDayOfMonth(), is(ld.getDayOfMonth()));
     }
@@ -58,7 +60,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} adjusted into first day of next year
          * by using {@link LocalDate#with} and {@link TemporalAdjusters#firstDayOfNextYear}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.with(TemporalAdjusters.firstDayOfNextYear());
 
         assertThat(localDate.getYear(), is(ld.getYear() + 1));
         assertThat(localDate.getMonth(), is(Month.JANUARY));
@@ -73,7 +75,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} with 10 month later
          * by using {@link LocalDate#plusMonths} or {@link LocalDate#plus}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.plusMonths(10);
 
         assertThat(localDate.getYear(), is(ld.getYear() + 1));
         assertThat(localDate.getMonth(), is(Month.APRIL));
@@ -88,7 +90,7 @@ public class Exercise1Test {
          * Create a {@link LocalDate} from {@link ld} with 10 days before
          * by using {@link LocalDate#minusDays} or {@link LocalDate#minus}
          */
-        LocalDate localDate = null;
+        LocalDate localDate = ld.minusDays(10);
 
         assertThat(localDate.getYear(), is(ld.getYear()));
         assertThat(localDate.getMonth(), is(ld.getMonth()));
@@ -104,8 +106,8 @@ public class Exercise1Test {
          * Define a {@link Period} of 1 year 2 month 3 days
          * Create a {@link LocalDate} adding the period to {@link ld} by using {@link LocalDate#plus}
          */
-        Period period = null;
-        LocalDate localDate = null;
+        Period period = Period.of(1, 2, 3);
+        LocalDate localDate = ld.plus(period);
 
         assertThat(localDate.getYear(), is(ld.getYear() + 1));
         assertThat(period.getMonths(), is(2));
@@ -121,7 +123,7 @@ public class Exercise1Test {
          * Check whether {@link ld2} is after {@link ld} or not
          * by using {@link LocalDate#isAfter} or {@link LocalDate#isBefore}
          */
-        boolean isAfter0618 = false;
+        boolean isAfter0618 = ld2.isAfter(ld);
 
         assertThat(isAfter0618, is(true));
     }
@@ -135,7 +137,7 @@ public class Exercise1Test {
          * Create a period from {@link ld} till {@link ld2}
          * by using {@link LocalDate#until}
          */
-        Period period = null;
+        Period period = ld.until(ld2);
 
         assertThat(period.getYears(), is(0));
         assertThat(period.getMonths(), is(1));
