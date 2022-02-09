1d81e17bbaa543f71dd4711f6fe8f7c82e7942db
ajil <ajil.qburst.com>
2022-02-09 19:51:45 +0530

    Done day1 work

--- collection-interfaces/src/test/java/collection/interfaces/Exercise1Test.java
+++ collection-interfaces/src/test/java/collection/interfaces/Exercise1Test.java
@@ -31,8 +31,10 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Iterate {@link customerIterable} with {@link Iterable#forEach} and use the {@link Consumer}
          * to finish creating the name list.
          */
-        Consumer<Object> consumer = null;
+        Consumer<Customer> consumer = customer -> nameList.add(customer.getName());
         // customerIterable.
+        customerIterable.forEach(consumer);
+
 
         assertThat(nameList.toString(), is("[Joe, Steven, Patrick, Diana, Chris, Kathy, Alice, Andrew, Martin, Amy]"));
     }
@@ -46,8 +48,9 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Create a {@link Predicate} which predicates if the input string contains "e".
          * Remove elements from {@link nameCollection} which contains "e" using {@link Collection#removeIf}.
          */
-        Predicate<Object> predicate = null;
+        Predicate<String> predicate = name -> name.contains("e");
         // nameCollection.
+        nameCollection.removeIf(predicate);
 
         assertThat(nameCollection.toString(), is("[Patrick, Chris]"));
     }
@@ -61,8 +64,9 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Create a {@link UnaryOperator} which returns given string wrapped with "()".
          * Replace the elements in {@link nameList} with string wrapped with "()" using {@link List#replaceAll} .
          */
-        UnaryOperator<Object> unaryOperator = null;
+        UnaryOperator<String> unaryOperator = name -> "("+ name +")";
         // nameList.
+        nameList.replaceAll(unaryOperator);
 
         assertThat(nameList.toString(), is("[(Joe), (Steven), (Patrick), (Chris)]"));
     }
@@ -75,8 +79,9 @@ public class Exercise1Test extends ClassicOnlineStore {
         /**
          * Create a {@link Comparator} to sort the name list by their name's length in ascending order.
          */
-        Comparator<Object> comparator = null;
+        Comparator<String> comparator = (a,b) -> a.length()-b.length();
         // nameList.
+        nameList.sort(comparator);
 
         assertThat(nameList.toString(), is("[Joe, Chris, Steven, Patrick]"));
     }
@@ -90,7 +95,7 @@ public class Exercise1Test extends ClassicOnlineStore {
          * Create a serial {@link Stream} using {@link Collection#stream}
          * You can learn about {@link Stream} APIs at stream-api module.
          */
-        Stream<Object> nameStream = null;
+        Stream<String> nameStream = nameList.stream();
 
         assertThat(nameStream.count(), is(4L));
         assertThat(nameStream.isParallel(), is(false));
@@ -104,7 +109,7 @@ public class Exercise1Test extends ClassicOnlineStore {
         /**
          * Create a parallel {@link Stream} using {@link Collection#parallelStream} or {@link Stream#parallel}
          */
-        Stream<Object> nameParallelStream = null;
+        Stream<String> nameParallelStream = nameList.parallelStream();
 
         assertThat(nameParallelStream.count(), is(4L));
         assertThat(nameParallelStream.isParallel(), is(true));
